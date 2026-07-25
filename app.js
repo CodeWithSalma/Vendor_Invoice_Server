bdfd835141bb371f74765bbc7be6a7ddaa5d7bed
Salma <sekolaonlen1@gmail.com>
2026-07-25 10:51:43 +0700

    remove double express

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const Invoice = require("./models/Invoice");
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
@@ -6,40 +5,25 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const connectDB = require("./config/database");
-
 const invoiceRoutes = require("./routes/invoiceRoutes");
 
 connectDB();
 
-const express = require("express");
 const app = express();
 
 app.use(cors({
-
     origin: [
-
         "http://localhost:5500",
-
         "https://vendor-invoice-client.vercel.app"
-
     ]
-
 }));
 
 app.use(express.json());
 
-app.use(cors());
-
-app.use(express.json());
-
 app.get("/", (req, res) => {
-
     res.send("Vendor Invoice API Running 🚀");
-
 });
 
 app.use("/api/invoices", invoiceRoutes);
-
-const PORT = process.env.PORT || 8443;
 
 module.exports = app;
