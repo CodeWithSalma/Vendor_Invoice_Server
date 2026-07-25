6cde5525fd5b70795f5055d8dbee970e1ec5d2a1
Salma <sekolaonlen1@gmail.com>
2026-07-25 11:04:20 +0700

    update gitignore

--- app.js
+++ app.js
@@ -1,45 +1,29 @@
-const Invoice = require("./models/Invoice");
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 
 const connectDB = require("./config/database");
-
 const invoiceRoutes = require("./routes/invoiceRoutes");
 
 dotenv.config();
 
 connectDB();
 
-const express = require("express");
 const app = express();
 
 app.use(cors({
-
     origin: [
-
         "http://localhost:5500",
-
         "https://vendor-invoice-client.vercel.app"
-
     ]
-
 }));
 
 app.use(express.json());
 
-app.use(cors());
-
-app.use(express.json());
-
 app.get("/", (req, res) => {
-
     res.send("Vendor Invoice API Running 🚀");
-
 });
 
 app.use("/api/invoices", invoiceRoutes);
-
-const PORT = process.env.PORT || 8443;
 
 module.exports = app;
