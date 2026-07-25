1c74739b4eddc85dbb5d9ec0003d0ffceac1ec23
Salma <sekolaonlen1@gmail.com>
2026-07-25 20:59:00 +0700

    troubleshoot

--- api/index.js
+++ api/index.js
@@ -4,6 +4,13 @@
 
 // module.exports = serverless(app);
 
+
+// module.exports = (req, res) => {
+//   res.json({ ok: true, msg: "bare handler works" });
+// };
+
+const app = require("../app");
+
 module.exports = (req, res) => {
-  res.json({ ok: true, msg: "bare handler works" });
+  res.json({ ok: true, msg: "app loaded", routes: app._router?.stack?.length });
 };
